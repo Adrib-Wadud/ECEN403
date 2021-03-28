12de3d68ccf18cf5f513a9bdb24577ec4878b4cc
jaredjanak <jaredjanak@tamu.edu>
2021-03-28 07:39:09 -0900

    ADC class for updated sensors5.py

--- MCP3008.py
+++ MCP3008.py
@@ -1,20 +1,26 @@
 from spidev import SpiDev
  
 class MCP3008:
+    
+    VREF = 5
+    
     def __init__(self, bus = 0, device = 0):
         self.bus, self.device = bus, device
+        #Initializing and characterizing SPI bus
         self.spi = SpiDev()
-        self.open()
-        self.spi.max_speed_hz = 1000000 # 1MHz
- 
-    def open(self):
-        self.spi.open(self.bus, self.device)
-        self.spi.max_speed_hz = 1000000 # 1MHz
+        self.spi.open(bus, device)
+        self.spi.max_speed_hz = 3900000 # 3.9MHz (MCP3008 supports up to 4.8MHz - 200ksps @ 24 clk pulses/sample)
     
-    def read(self, channel = 0):
-        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
-        data = ((adc[1] & 3) << 8) + adc[2]
-        return data
+    def getVoltage(self, channel): #reads voltage from specified channel
+        rawData = self.spi.xfer([1, (8 + channel) << 4, 0])
+        processedData = ((rawData[1]&3) << 8) + rawData[2]
+        voltage = (processedData / 1024) * self.VREF
+        return voltage
+
+    def convertToTemp(self, voltage, decimalPlaces = 2): #converts voltage to rounded temperature value
+        temperature = voltage / 0.010 #10mV/C (from LM35 temp sensor datasheet)
+        temperature = round(temperature, decimalPlaces)
+        return temperature
             
     def close(self):
         self.spi.close()
