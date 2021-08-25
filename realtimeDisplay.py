5c6b4ae8538dd9303c864ae18615abc910ecb9db
jaredjanak <81063776+jaredjanak@users.noreply.github.com>
2021-08-25 13:57:13 -0500

    Emailed myself the file

--- realtimeDisplay.py
+++ realtimeDisplay.py
@@ -9,22 +9,18 @@ from sys import exit
 SENSOR_UPDATE_TIME = 1 #seconds
 LCD_UPDATE_TIME = 0.1 #seconds
 
-#Human/Device Interaction Subsystem
-#initialize physical user interface
+#initialize all button and switch functionality
 physicalInput = physicalInputAssemblage()
 
 #instantiate LCD object
 lcd = LCD()
 
-
-#Sensor Subsystem
 #initialize ADC
 adc = MCP3008()
 
-#defining variables for use in main program loop
+#define variables for use in main program loop
 temperatureChannel = 0 #ADC channel connected to temp sensor
 temperatureSum = 0 #running sum of temp sensor readings (averaged to smooth sensor output)
-
 humidityChannel = 1 #ADC channel connected to humidity sensor
 humiditySum = 0 #running sum of humidity sensor readings (averaged to smooth sensor output)
 
@@ -37,29 +33,32 @@ humidity = round(adc.getHumidity(humidityChannel, temperature), 1) #reporting hu
 
 #placeholder for Nanda's code
 batteryLevel = 50
+batteryVoltage = 1
+batteryAmperage = 2
+panelVoltage = 3
+panelWattage = 4
 
 while True:
     try:
-        #Physical Button Assemblage
         #performing button specific increments/decrements on button press events
         physicalInput.fan_TempUpButton.when_pressed = physicalInput.incrementFan_Temp
         physicalInput.fan_TempDownButton.when_pressed = physicalInput.decrementFan_Temp
         physicalInput.humidifier_HumidityUpButton.when_pressed = physicalInput.incrementHumidifier_Humidity
         physicalInput.humidifier_HumidityDownButton.when_pressed = physicalInput.decrementHumidifier_Humidity
+        physicalInput.toggleDisplayButton.when_pressed = physicalInput.toggleDisplay
         physicalInput.manualAutoSwitch.when_pressed = physicalInput.switchToAuto
         physicalInput.manualAutoSwitch.when_released = physicalInput.switchToManual
         
-        
         #Sensor Reading Updates
-        #counting each temperature and humidity reading and adding to individual sums in preparation for averaging
+        #counting number of temperature and humidity readings and adding to individual sums in preparation for averaging
         currentTemperature = adc.getTemperature(temperatureChannel)
         temperatureSum += currentTemperature
-        currentHumidity = adc.getHumidity(humidityChannel, currentTemperature)
-        humiditySum += currentHumidity
+        humiditySum += adc.getHumidity(humidityChannel, currentTemperature)
         numReadings += 1
         
+        #after every second, calculate sensor data averages
         presentTime = time()
-        if((presentTime - pastSensorTime) >= SENSOR_UPDATE_TIME): #after every second, calculate sensor data averages
+        if((presentTime - pastSensorTime) >= SENSOR_UPDATE_TIME):
             #average temperature and humidity readings and round to one decimal place
             temperature = round(temperatureSum / numReadings, 1)
             humidity = round(humiditySum / numReadings, 1)
@@ -68,17 +67,17 @@ while True:
             temperatureSum, humiditySum, numReadings = 0, 0, 0
             pastSensorTime = presentTime
         
-        
         #LCD Screen Updates
-        if((presentTime - pastSensorTime) >= LCD_UPDATE_TIME): #after every 0.1 second, update LCD
-            #display data on screen depending on selected mode
-            lcd.displaySystemData(temperature, humidity, physicalInput.fanSpeed,
+        if((presentTime - pastSensorTime) >= LCD_UPDATE_TIME): #after every 0.1 second, update LCD display
+                                                               #data on screen depending on selected mode
+            lcd.displaySystemData(temperature, humidity, physicalInput.fanSpeed,  #write new display text to LCD
                     physicalInput.humidifierIntensity, physicalInput.tempSetting,
-                    physicalInput.humidityCap, batteryLevel, physicalInput.autoMode)
+                    physicalInput.humidityCap, physicalInput.displayScreen,
+                    physicalInput.autoMode, batteryLevel, batteryVoltage,
+                    batteryAmperage, panelVoltage, panelWattage)
             
     except KeyboardInterrupt: #allow user to terminate program with ctrl + c
-        #clear screen
-        lcd.lcd.clear()
-        cleanup()
+        lcd.lcd.clear()  #clear LCD
+        cleanup()  #set all output pins back to input pins
         
         exit("Program Terminated")
