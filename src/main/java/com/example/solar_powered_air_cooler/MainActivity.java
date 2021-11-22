ba5593a73f88845d8349759d12f3b26b59e535a0
adribwadud <adribwadud@gmail.com>
2021-11-22 12:37:08 -0600

    App working with dummy temp and humidity variables

--- src/main/java/com/example/solar_powered_air_cooler/MainActivity.java
+++ src/main/java/com/example/solar_powered_air_cooler/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.TextView;
@@ -17,7 +19,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     public static BluetoothSocket btSocket = null;
@@ -52,9 +56,18 @@ public class MainActivity extends AppCompatActivity {
 
         BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
         //B8:27:EB:FD:68:D8
-        BluetoothDevice device = btAdapter.getRemoteDevice("DC:A6:32:F9:96:16");
+        //DC:A6:32:F9:96:16
+        //B8:27:EB:81:5E:F2
+        Set<BluetoothDevice> set = btAdapter.getBondedDevices();
+        for (BluetoothDevice device : set){
+            Log.e("MyActivity", device.getAddress());
+        }
+
+        BluetoothDevice device = btAdapter.getRemoteDevice("B8:27:EB:81:5E:F2");
         //94f39d29-7d6d-437d-973b-fba39e49d4ee
+        //163660a6-ad17-44fc-99c5-5c75e78ad815
         UUID uuid = UUID.fromString("163660a6-ad17-44fc-99c5-5c75e78ad815");
+        //UUID uuid = device.getUuids()[0].getUuid();
         btSocket = null;
 
         try{
@@ -64,22 +77,25 @@ public class MainActivity extends AppCompatActivity {
                  Toast toast = Toast.makeText(this, "Connected", Toast.LENGTH_SHORT);
                  toast.show();
                  TextView connected = (TextView)findViewById(R.id.textView10);
+                 connected.setTextColor(Color.GREEN);
                  connected.setText("Yes");
 
-                 sendMessageToServer("Get current temperature!");
-                 String current_temperature = recvdMessageFromServer();
-                 TextView curr_temp_reading = (TextView)findViewById(R.id.curr_temp);
-                 curr_temp_reading.setText(current_temperature);
+                         sendMessageToServer("Get current temperature!");
+                         String current_temperature = recvdMessageFromServer();
+                         TextView curr_temp_reading = (TextView)findViewById(R.id.curr_temp);
+                         curr_temp_reading.setText(current_temperature);
+                         Log.e("MyActivity", current_temperature);
 
-                 sendMessageToServer("Get current humidity!");
-                 String current_humidty = recvdMessageFromServer();
-                 TextView curr_humid_reading = (TextView)findViewById(R.id.curr_humidity);
-                 curr_humid_reading.setText(current_humidty);
+                         sendMessageToServer("Get current humidity!");
+                         String current_humidty = recvdMessageFromServer();
+                         TextView curr_humid_reading = (TextView)findViewById(R.id.curr_humidity);
+                         curr_humid_reading.setText(current_humidty);
 
-                 sendMessageToServer("Get current charge!");
-                 chargeLevel = recvdMessageFromServer();
-                 TextView charge_level = (TextView)findViewById(R.id.charge_level);
-                 charge_level.setText(chargeLevel);
+                         sendMessageToServer("Get current charge!");
+                         chargeLevel = recvdMessageFromServer();
+                         chargeLevel = chargeLevel + '%';
+                         TextView charge_level = (TextView)findViewById(R.id.charge_level);
+                         charge_level.setText(chargeLevel);
 
 
              }
@@ -101,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             btSocket.close();
             TextView connected = (TextView)findViewById(R.id.textView10);
+            connected.setTextColor(Color.RED);
             connected.setText("No");
         } catch (IOException e) {
             e.printStackTrace();
